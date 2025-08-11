3ed4b7ccb2d5b8cfef69ece02c9cacae7053cbf9
Anton Nachodka <krygant8484@gmail.com>
2025-08-11 17:44:05 +1000

    Add RecyclerView

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,11 +1,11 @@
 package ru.netology.nmedia.dto
 
 data class Post(
-    val id: Int,
+    val id: Long,
     val author: String,
     val published: String,
     val content: String,
-    val like: Int = 9_999_999,
+    val likes: Int = 9_999_999,
     val share: Int = 999,
-    val likeByMe: Boolean = false
+    val likedByMe: Boolean = false
 )
