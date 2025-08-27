ee528196224c49f7e6f25dbfd1c50e96736c3ab3
Anton Nachodka <krygant8484@gmail.com>
2025-08-27 14:20:05 +1000

    SharedPrefsImpl/FilesImpl

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySharedPrefsImpl.kt
@@ -0,0 +1,87 @@
+package ru.netology.nmedia.repository
+
+//class PostRepackage ru.netology.nmedia.repository
+
+import android.content.Context
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.nmedia.dto.Post
+
+class PostRepositorySharedPrefsImpl(context: Context) : PostRepository {
+
+    private val prefs = context.getSharedPreferences("repo", Context.MODE_PRIVATE)
+
+    private var nextId = 1L
+    private var posts = listOf<Post>()
+        set(value) {
+            field = value
+            sync()
+        }
+
+    private val data = MutableLiveData(posts)
+
+    init {
+        prefs.getString(KEY_POSTS, null)?.let {
+            posts = gson.fromJson(it, type)
+            nextId = posts.maxOfOrNull { it.id }?.inc() ?: 1
+            data.value = posts
+        }
+    }
+
+    private fun sync() {
+        prefs.edit() {
+            putString(KEY_POSTS, gson.toJson(posts))
+        }
+    }
+
+    override fun get(): LiveData<List<Post>> = data
+
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = if (it.likedByMe) it.likes - 1 else it.likes + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareById(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                shareByMe = true,
+                share = it.share + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun removeById(id: Long) {
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+
+    override fun save(post: Post) {
+        posts = if (post.id == 0L) {
+            listOf(post.copy(id = nextId++, author = "Me", published = "now")) + posts
+        } else {
+            posts.map { if (it.id != post.id) it else it.copy(content = post.content) }
+        }
+
+        data.value = posts
+    }
+
+    override fun getPlayVideo(id: Long): String? {
+        return posts.find { it.id == id }?.video
+    }
+
+    companion object {
+        private const val KEY_POSTS = "posts"
+
+        private val gson = Gson()
+        private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    }
+}
