41e57dd60e9b96b2e427a74996605ca4d86848e4
Anton Nachodka <krygant8484@gmail.com>
2025-09-05 17:09:31 +1000

    Add SQLite

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,11 +4,10 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun get(): LiveData<List<Post>>
+    fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun shareById(id: Long)
     fun removeById(id: Long)
     fun save(post: Post)
     fun getPlayVideo(id: Long): String?
-    fun getPostById(id: Long): Post?
 }
